1fcd7a0ed111594bc18d11a7c76bf0a21bc0d7a7
nurwachidaji034 <nugrahaaji402@gmail.com>
2023-06-14 00:02:01 +0800

    Membuat class MatematikaBeraksi

--- src/Hitung/Matematika.java
+++ src/Hitung/Matematika.java
@@ -1,14 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hitung;
 
-/**
- *
- * @author User
- */
 public class Matematika {
+    private double bil1;
+    private double bil2;
     
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+    
+    public double setPenjumlahan() {
+        return bil1 + bil2;
+    }
+    
+    public double setPengurangan() {
+        return bil1 - bil2;
+    }
+    
+    public double setPerkalian() {
+        return bil1 * bil2;
+    }
+    
+    public double setPembagian() {
+        return bil1 / bil2;
+    }
 }
--- src/Hitung/MatematikaBeraksi.java
+++ src/Hitung/MatematikaBeraksi.java
@@ -1,14 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Hitung;
 
-/**
- *
- * @author User
- */
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika Aji = new Matematika(0, 58); // Ganti 23 dengan dua digit terakhir NPM Anda
+        
+        double hasilPenjumlahan = Aji.setPenjumlahan();
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+        
+        double hasilPengurangan = Aji.setPengurangan();
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+        
+        double hasilPerkalian = Aji.setPerkalian();
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+        
+        double hasilPembagian = Aji.setPembagian();
+        System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
 }
